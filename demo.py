ae0ff45dad40bb772940c01852f76e8903c1914f
snehasharma2541 <snehasharma2541@gmail.com>
2025-07-01 23:09:25 +0530

    updates

---
+++ demo.py
@@ -0,0 +1,2 @@
+str = 'I am a student of machine learning at Aptech Learning Institute.'
+print(str)
--- learning.py
+++ learning.py
@@ -1,1 +1,3 @@
 print("Failures are the stepping stone to success.")
+
+print("The secret of getting ahead is getting started.")
