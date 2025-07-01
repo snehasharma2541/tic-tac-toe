14fc80510e19bf50fb3267548822de46c32d01eb
snehasharma2541 <snehasharma2541@gmail.com>
2025-07-01 23:33:21 +0530

    fix-code

--- demo.py
+++ demo.py
@@ -1,2 +1,15 @@
 str = 'I am a student of machine learning at Aptech Learning Institute.'
 print(str)
+
+
+def fibonacci_series(n):
+    first_term = 0
+    second_term = 1
+    next_term = 0
+    for i in range(n):
+        print(first_term)
+        next_term = first_term + second_term
+        first_term = second_term
+        second_term = next_term
+        
+fibonacci_series(10)
